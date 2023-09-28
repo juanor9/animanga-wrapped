ac24f4d61952565089751ca477579c8c4885078e
Juan Camilo Orjuela <orjuela9@gmail.com>
2023-09-28 11:26:36 -0500

    set basic query to get user's activities'

--- src/app/anilist/services/queries.js
+++ src/app/anilist/services/queries.js
@@ -11,3 +11,61 @@ export const GET_VIEWER = gql`
     }
   }
 `;
+
+export const GET_LIST = gql`
+query ($page: Int, $perPage: Int, $userId: Int) {
+
+Page(page: $page, perPage: $perPage) {
+  pageInfo {
+    total
+    currentPage
+    lastPage
+    hasNextPage
+    perPage
+  }
+  activities(userId: $userId, type:MEDIA_LIST, createdAt_greater: 1672531200, createdAt_lesser: 1704067199, sort: ID_DESC) {
+    ... on ListActivity {
+      createdAt
+      type
+      progress
+      status
+      media {
+        title {
+          userPreferred
+        }
+        id
+        duration
+        genres
+        tags {
+          id
+          name
+        }
+        studios(isMain: true) {
+          nodes {
+            id
+            name
+            isAnimationStudio
+          }
+        }
+        staff {
+          nodes {
+            id
+            primaryOccupations
+            gender
+            name {
+              first
+              middle
+              last
+              full
+              native
+              userPreferred
+            }
+            
+          }
+        }
+      }
+    }
+  }
+}
+}
+`
