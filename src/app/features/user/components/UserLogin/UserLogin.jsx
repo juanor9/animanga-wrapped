bc314bb0bf19d71bfaf40af3bad6635c98334a40
Juan Camilo Orjuela <orjuela9@gmail.com>
2023-11-03 09:12:54 -0500

    redirect users to user page after login in

--- src/app/features/user/components/UserLogin/UserLogin.jsx
+++ src/app/features/user/components/UserLogin/UserLogin.jsx
@@ -1,19 +1,32 @@
-/* eslint-disable no-unused-vars */
+// Importando las librerías y dependencias necesarias
 import { useDispatch } from 'react-redux';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/navigation';
 import { login } from '../../services/users';
 
 const UserLogin = () => {
+  // Hooks de Redux y Next.js
   const dispatch = useDispatch();
+  const router = useRouter();
+
+  // Estado local para manejar el email y password del formulario
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+
+  // Estado local para determinar si debemos redireccionar al usuario después del login
+  const [shouldRedirect, setShouldRedirect] = useState(false);
+
+  // Manejador de cambio para el email
   const handleEmailChange = (event) => {
     setEmail(event.target.value);
   };
+
+  // Manejador de cambio para el password
   const handlePasswordChange = (event) => {
     setPassword(event.target.value);
   };
 
+  // Manejador de envío del formulario
   const handleSubmit = async (event) => {
     event.preventDefault();
 
@@ -22,18 +35,24 @@ const UserLogin = () => {
         email,
         password,
       };
-      dispatch(login(form));
-      // navigate('/catalogue');
+      // Dispatch de la acción de login y luego seteamos el redireccionamiento
+      dispatch(login(form)).then(() => setShouldRedirect(true));
     } catch (error) {
       throw new Error(error);
     }
   };
+
+  // Efecto para manejar el redireccionamiento cuando el usuario ha iniciado sesión
+  useEffect(() => {
+    if (shouldRedirect) {
+      router.push('./user');
+    }
+  }, [shouldRedirect]);
+
+  // Renderizado del componente
   return (
     <section className="user-registration">
-      <form
-        onSubmit={handleSubmit}
-        className="user-registration__form"
-      >
+      <form onSubmit={handleSubmit} className="user-registration__form">
         <label htmlFor="mail" className="user-registration__label">
           Email
           <input
@@ -41,7 +60,7 @@ const UserLogin = () => {
             id="mail"
             name="mail"
             onChange={handleEmailChange}
-            type="mail"
+            type="email" // Mantuve el tipo como 'email' porque es el tipo correcto para este tipo de input
           />
         </label>
         <label htmlFor="password" className="user-registration__label">
