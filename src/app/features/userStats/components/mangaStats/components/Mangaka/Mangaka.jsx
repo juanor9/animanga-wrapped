55bad2812d7be5c3ee109eb4d33dcaaed3015b7e
Juan Camilo Orjuela <orjuela9@gmail.com>
2023-12-26 11:45:49 -0500

    fix images in storycards

--- src/app/features/userStats/components/mangaStats/components/Mangaka/Mangaka.jsx
+++ src/app/features/userStats/components/mangaStats/components/Mangaka/Mangaka.jsx
@@ -1,10 +1,16 @@
 import { useState, useEffect } from 'react';
 import { v4 as uuidv4 } from 'uuid';
+import { useDispatch, useSelector } from 'react-redux';
+import uploadImage from '../../../../services/upload';
 import StoryCard from '../../../../../../components/Stories/Stories';
 // TODO: realizar el conteo, no por cantidad de actividades, sino por cantidad de capítulos leídos.
 
+const serverUrl = process.env.NEXT_PUBLIC_REACT_APP_BASE_URL;
+
 const Mangaka = ({ list }) => {
   const [mangakaList, SetMangakaList] = useState([]);
+  const dispatch = useDispatch();
+  const { listUsername } = useSelector((state) => state.UserReducer.user);
 
   useEffect(() => {
     if (list) {
@@ -38,7 +44,7 @@ const Mangaka = ({ list }) => {
           {
             id: uuidv4(),
             mangaka: e[0],
-            img: e[1].image,
+            image: e[1].image,
             count: e[1].titles,
           }
         ),
@@ -47,21 +53,91 @@ const Mangaka = ({ list }) => {
       SetMangakaList(mangakaListSorted);
     }
   }, [list]);
+
+  const [topMangaka, setTopMangaka] = useState([]);
+  const downloadToCloudinary = async (url, filename) => {
+    try {
+      const response = await fetch(url);
+      if (!response.ok) { throw new Error(`HTTP error! status: ${response.status}`); }
+
+      const blob = await response.blob();
+      const file = new File([blob], 'anime-watched-image.png', {
+        type: blob.type,
+      });
+
+      const uploadedImageResponse = await dispatch(
+        uploadImage({ file, listUsername, filename }),
+      );
+
+      if (uploadedImageResponse.type === 'uploads/uploadImage/fulfilled') {
+        const cloudinaryUrl = uploadedImageResponse.payload.url;
+        return cloudinaryUrl;
+      }
+      throw new Error('Image upload failed');
+    } catch (error) {
+      throw new Error('Error downloading or uploading image:', error);
+    }
+  };
+
+  useEffect(() => {
+    const processImages = async () => {
+      if (mangakaList && mangakaList.length > 0) {
+        const rawTopWatchedMinutes = mangakaList.slice(0, 5);
+
+        const newTopWatchedMinutesPromises = rawTopWatchedMinutes.map(
+          async (element) => {
+            const alImage = element.image;
+            const parts = alImage.split('/');
+            const newPath = parts.slice(3).join('/');
+            const newUrl = `${serverUrl}/api/al/sources/${newPath}`;
+            const cloudinaryUrl = await downloadToCloudinary(
+              newUrl,
+              element.mangaka,
+            );
+
+            return {
+              ...element,
+              image: cloudinaryUrl,
+            };
+          },
+        );
+
+        const newTopWatchedMinutes = await Promise.all(
+          newTopWatchedMinutesPromises,
+        );
+
+        setTopMangaka(newTopWatchedMinutes);
+      }
+    };
+
+    processImages();
+  }, [mangakaList]);
+
   return (
     <StoryCard key="13" id="13" color="orange">
       <p className="story__main-copy">This were the mangaka you most read this year:</p>
       <ol className="story__list-container">
-        {mangakaList
-          ? mangakaList.slice(0, 5).map((item) => (
-            <li key={item.id} className="story__list-item">
-              <picture className="story__list-image">
-                <img src={item.img} alt={item.mangaka} />
-              </picture>
-              <div className="story__list-text">
-                <p className="story__list-text--title">{item.mangaka}</p>
-              </div>
-            </li>
-          ))
+        {topMangaka
+          ? topMangaka.slice(0, 5).map((item) => {
+            const url = item.image;
+            const cloudinaryParams = 'ar_1:1,c_crop/ar_1:1,c_scale,w_300/';
+            const parts = url.split('image/upload/');
+            const image = `${parts[0]}image/upload/${cloudinaryParams}${parts[1]}`;
+            return (
+              <li key={item.id} className="story__list-item">
+                <picture className="story__list-image">
+                  <div
+                    role="img"
+                    aria-label={item.mangaka}
+                    style={{ backgroundImage: `url(${image})` }}
+                  />
+                </picture>
+                <div className="story__list-text">
+                  <p className="story__list-text--title">{item.mangaka}</p>
+                </div>
+              </li>
+            );
+          })
           : null}
       </ol>
     </StoryCard>
