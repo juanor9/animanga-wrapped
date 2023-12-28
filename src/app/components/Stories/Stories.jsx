ceaae451abb71bcecaf48f72e7c181a824853e98
Juan Camilo Orjuela <orjuela9@gmail.com>
2023-12-27 20:22:32 -0500

    set spinner for uploading image

--- src/app/components/Stories/Stories.jsx
+++ src/app/components/Stories/Stories.jsx
@@ -1,11 +1,17 @@
 import { useEffect, useRef, useState } from 'react';
 import html2canvas from 'html2canvas';
+import { useSelector, useDispatch } from 'react-redux';
 import './Stories.scss';
+import uploadImage from '../../features/userStats/services/upload';
+import Spinner from '../Spinner/Spinner';
 
-const StoryCard = ({ children, color }) => {
+const StoryCard = ({ children, color, id }) => {
   const storyCardRef = useRef();
   const [imagesLoaded, setImagesLoaded] = useState(0);
   const [totalImages, setTotalImages] = useState(0);
+  const [isUploading, setIsUploading] = useState(false);
+  const { listUsername } = useSelector((state) => state.UserReducer.user);
+  const dispatch = useDispatch();
 
   const captureOptions = {
     allowTaint: true,
@@ -31,40 +37,50 @@ const StoryCard = ({ children, color }) => {
 
   const allImagesLoaded = imagesLoaded === totalImages;
 
-  const saveStoryCard = async () => {
+  const uploadAndSaveStoryCard = async () => {
+    setIsUploading(true);
     try {
       const canvas = await html2canvas(storyCardRef.current, captureOptions);
-      // Convertir el lienzo a Blob en lugar de Data URL
-      canvas.toBlob((blob) => {
-        // Crear un objeto URL con el Blob
-        const imageUrl = URL.createObjectURL(blob);
-        // Abrir la imagen en una nueva pestaña
-        window.open(imageUrl);
+      canvas.toBlob(async (blob) => {
+        const file = new File([blob], 'storycard.png', { type: 'image/png' });
+        const uploadData = {
+          file,
+          listUsername, // Asegúrate de que listUsername esté definido
+          filename: `${listUsername}-${id}`, // Este es opcional
+        };
+        const resultAction = await dispatch(uploadImage(uploadData));
+        const data = resultAction.payload;
+
+        if (data && data.url) {
+          window.open(data.url, '_blank');
+        }
+        setIsUploading(false);
       });
     } catch (error) {
-      console.error('Error saving the story card:', error);
+      console.error('Error uploading the story card:', error);
+      setIsUploading(false);
     }
   };
 
   return (
     <div className="carrusel__item">
-      <div>
-        <div ref={storyCardRef} id="series-story" className={`story story--${color}-gradient`}>
-          <div className="story__content">
-            {children}
-          </div>
-          <div className="story__footer">
-            <p className="story__footer-link">animanga-wrapped.vercel.app</p>
-          </div>
+      <div ref={storyCardRef} id="series-story" className={`story story--${color}-gradient`}>
+        <div className="story__content">
+          {children}
         </div>
-        {allImagesLoaded
-          ? (
-            <div className="story__button-container">
-              <button type="button" onClick={saveStoryCard} className={`story__button story__button--${color}`}>Save</button>
-            </div>
-          )
-          : null}
+        <div className="story__footer">
+          <p className="story__footer-link">animanga-wrapped.vercel.app</p>
+        </div>
       </div>
+      {allImagesLoaded && (
+        <div className="story__button-container">
+          {isUploading ? (
+            <Spinner />
+          ) : (
+            <button type="button" onClick={uploadAndSaveStoryCard} className={`story__button story__button--${color}`}>Save</button>
+          )}
+        </div>
+      )}
     </div>
   );
 };
