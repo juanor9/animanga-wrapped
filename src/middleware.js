ba5327e5f8dc7cffb1ae79201388fefbe97d3454
Juan Camilo Orjuela <orjuela9@gmail.com>
2025-12-14 16:57:46 -0500

    Fix/import issues 92 (#95)
    * Fix build issues
    
    * feat: Implement AniList/MAL integration, user registration, Redux state, and Next.js performance optimizations.
    
    * feat: Add Next.js internationalization middleware with en and es locales.

--- src/middleware.js
+++ src/middleware.js
@@ -1,12 +1,11 @@
 import createMiddleware from 'next-intl/middleware';
-import { defaultLocale, locales } from '../i18n';
 
 export default createMiddleware({
   // A list of all locales that are supported
-  locales,
+  locales: ['en', 'es'],
 
   // Used when no locale matches
-  defaultLocale,
+  defaultLocale: 'en',
 
   // Always use locale prefix
   localePrefix: 'always',
